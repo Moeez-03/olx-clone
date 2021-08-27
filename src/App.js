ea19e37f6c40427a5e1a05f030a0505287bb5b3f
Moeez03 <moeezabeeha1@gmail.com>
2021-08-28 00:11:00 +0500

    base-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+  
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+import Create from './Pages/Create';
+import ProductPage from './Pages/ProductPage';
+import MyAdsPage from './Pages/MyAdsPage';
+import SearchPage from './Pages/SearchPage';
+import PageNotFound from './Pages/PageNotFound';
+import ChatPage from './Pages/ChatPage';
+import MyProfilePage from './Pages/MyProfilePage';
+import EditPage from './Pages/EditPage';
+import SellerProfile from './Pages/SellerProfile';
+import EditPostPage from './Pages/EditPostPage';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path="/create">
+            <Create />
+          </Route>
+          <Route path="/item/:productId">
+            <ProductPage />
+          </Route>
+          <Route path="/search/:searchId">
+            <SearchPage />
+          </Route>
+          <Route path='/myads' component={MyAdsPage} />
+          <Route path='/myfavorites' component={MyAdsPage} />
+          <Route path='/chat/:chatId' component={ChatPage} />
+          <Route path='/myprofile' component={MyProfilePage} />
+          <Route path='/editprofile/:editInfo' component={EditPage} />
+          <Route path='/profile/:profileId' component={SellerProfile} />
+          <Route path='/editpost/:postId' component={EditPostPage} />
+          <Route component={PageNotFound} path='*' />
+        </Switch>
+      </Router>
     </div>
   );
 }
